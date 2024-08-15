1317e159cbb3676ff837c13e215a5847f882301b
Yuki Tantei <36851559+SLIIV@users.noreply.github.com>
2024-08-16 03:04:38 +0800

    register css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,68 @@
-<x-guest-layout>
+@extends('layouts.navigation')
+@section('content')
+<div class="container jost-weight">
+    <div class="main-description text-center">
+        Регистрация
+    </div>
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        
+       <div class="row pt-5">
+            <div class="col-md-2 mx-auto">
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" class="form-control" placeholder="Имя" :value="old('name')" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+       </div>
+        
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="row mt-3">
+            <div class="col-md-2 mx-auto">
+                <x-text-input id="email" class="block mt-1 w-full" type="email" placeholder="Почта" name="email" class="form-control" :value="old('email')" required autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="row mt-3">
+            <div class="col-md-2 mx-auto">
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                class="form-control"
+                                placeholder="Пароль"
+                                required autocomplete="new-password" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div class="row mt-3">
+            <div class="col-md-2 mx-auto">
+                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation"
+                                placeholder="Повтор пароля"
+                                class="form-control" required autocomplete="new-password" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="row mt-3">
+            <div class="col-md-2 mx-auto">
+                    <x-primary-button class="bg-primary form-control">
+                       Регистрация
+                    </x-primary-button>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-2 mx-auto text-center">
+                <a class="text-white" href="{{ route('login') }}">
+                    {{ __('Уже зарегистрированы?') }}
+                </a>
+            </div>
         </div>
     </form>
-</x-guest-layout>
+    </div>
+@endsection
