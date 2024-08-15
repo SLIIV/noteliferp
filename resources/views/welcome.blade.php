45570e1b8b7ac02a35be9031623c78423e088b41
Yuki Tantei <36851559+SLIIV@users.noreply.github.com>
2024-08-16 04:26:11 +0800

    ya verify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="{{ asset('build/assets/styles.css') }}" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <meta name="yandex-verification" content="97dd009a02b03990" />
     </head>
     <body class="blue">
         <header>
