a5ed23f4542adaf954a320749b2fe35fe798d893
Yuki Tantei <36851559+SLIIV@users.noreply.github.com>
2024-08-16 17:12:23 +0800

    titles changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Note Life - лучший инструмент финансовый инструмент в мире GTA 5 RP</title>
+        <title>NOTELIFE - лучший калькулятор прибыли GTA 5 RP</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
